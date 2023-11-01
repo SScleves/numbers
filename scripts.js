9b0dee127ce5a32bb4dfd2f725c2f4eef2e67c37
SScleves <99815935+SScleves@users.noreply.github.com>
2023-11-01 18:25:28 +0100

    Add new API functions to script.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 function getNumberFact() {
     let number = document.getElementById("number-input").value;
-
-    // Using the Numbers API for this example.
-    let apiURL = `http://numbersapi.com/${number}`;
+    let apiURL = `https://numbersapi.com/${number}`;
 
     fetch(apiURL)
         .then(response => response.text())
@@ -11,5 +9,48 @@ function getNumberFact() {
         })
         .catch(error => {
             console.error("Error fetching the number fact:", error);
+        });
+}
+
+function getChuckNorrisJoke() {
+    let apiURL = 'https://api.chucknorris.io/jokes/random';
+
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("chuckNorrisJoke").textContent = data.value;
+        })
+        .catch(error => {
+            console.error("Error fetching the Chuck Norris joke:", error);
+        });
+}
+
+function getCatFact() {
+    let apiURL = 'https://catfact.ninja/fact';
+
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("catFact").textContent = data.fact;
+        })
+        .catch(error => {
+            console.error("Error fetching the cat fact:", error);
+        });
+}
+
+function getDadJoke() {
+    let apiURL = 'https://icanhazdadjoke.com/';
+
+    fetch(apiURL, {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("dadJoke").textContent = data.joke;
+        })
+        .catch(error => {
+            console.error("Error fetching the dad joke:", error);
         });
 }
