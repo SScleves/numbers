2bc88b0f2bc02eee72b6ac7bf3295b3c7c119133
SScleves <99815935+SScleves@users.noreply.github.com>
2023-11-02 17:50:11 +0100

    Description of the changes switch

--- scripts.js
+++ scripts.js
@@ -13,3 +13,31 @@ document.getElementById("fetchCatImage").addEventListener("click", function() {
             document.getElementById("catImage").src = data[0].url;
         });
 });
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Get Chuck Norris Joke
+    const chuckJokeButton = document.getElementById('chuckNorrisJoke');
+    const jokeText = document.getElementById('jokeText');
+  
+    chuckJokeButton.addEventListener('click', () => {
+      fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => response.json())
+        .then(data => {
+          jokeText.textContent = data.value; // Display the joke
+        })
+        .catch(error => console.error('Error fetching Chuck Norris joke:', error));
+    });
+  
+    // Get Cat Fact
+    const catFactButton = document.getElementById('catFact');
+    const factText = document.getElementById('factText');
+  
+    catFactButton.addEventListener('click', () => {
+      fetch('https://catfact.ninja/fact')
+        .then(response => response.json())
+        .then(data => {
+          factText.textContent = data.fact; // Display the cat fact
+        })
+        .catch(error => console.error('Error fetching cat fact:', error));
+    });
+  });
+  
